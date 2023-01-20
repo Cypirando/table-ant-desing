c0e7c3ca290598dae04b64fa7d82fa679d079fdf
cypirando <fabiocypriano@gmail.com>
2023-01-20 17:40:20 -0300

    pequenas mudanças e correção de bugs

---
+++ src/components/Input/index.jsx
@@ -0,0 +1,19 @@
+// import { Input } from 'antd';
+
+// function handleChange(e, field, id) {
+//   // lógica para lidar com a alteração do valor do campo
+// }
+
+// const EditableTableColumn = ({ text, record }) => {
+//   return (
+//     <>
+//       {record.editable ? (
+//         <Input defaultValue={text} onChange={e => handleChange(e, 'name', record.id)} />
+//       ) : (
+//         text
+//       )}
+//     </>
+//   );
+// };
+
+// export default EditableTableColumn;
---
+++ src/components/Tabela/index.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import { Table, Modal, Input, Button } from "antd";
+
+const data = {
+  restaurant_id: "54321",
+  menu: [
+    {
+      id: "1",
+      name: "Steak Frites",
+      description: "Bife grelhado acompanhado de batatas fritas",
+      price: "22.99",
+      attributes: ["carne", "batatas fritas", "grelhado"],
+    },
+    {
+      id: "2",
+      name: "Poulet à la crème",
+      description: "Frango em molho de creme e ervas",
+      price: "18.99",
+      attributes: ["frango", "creme", "ervas"],
+    },
+    {
+      id: "3",
+      name: "Sushi Maki",
+      description: "Conjunto de makis de peixe e vegetais",
+      price: "15.99",
+      attributes: ["peixe", "vegetais", "sushi"],
+    },
+    {
+      id: "4",
+      name: "Paella Valenciana",
+      description: "Paella de frutos do mar e arroz",
+      price: "21.99",
+      attributes: ["frutos do mar", "arroz"],
+    },
+    {
+      id: "5",
+      name: "Gnocchi al Pesto",
+      description: "Gnocchi em molho de pesto de manjericão",
+      price: "16.99",
+      attributes: ["gnocchi", "pesto", "manjericão"],
+    },
+  ],
+};
+
+function Restaurante() {
+  const [visivel, setVisivel] = useState(false);
+  const [mudaDado, setMudaDado] = useState({});
+  const [menu, setMenu] = useState(data.menu);
+
+  const coluna = [
+    {
+      title: "Nome",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Descrição",
+      dataIndex: "description",
+      key: "description",
+    },
+    {
+      title: "Preço",
+      dataIndex: "price",
+      key: "price",
+    },
+    {
+      title: "Ação",
+      key: "action",
+      render: (text, record) => (
+        <Button type="link" onClick={() => editaTabela(record)}>
+          Editar
+        </Button>
+      ),
+    },
+  ];
+
+  function editaTabela(record) {
+    setMudaDado(record);
+    setVisivel(true);
+  }
+
+  function salvaTabela() {
+    setMenu(
+      menu.map((item) => {
+        if (item.id === mudaDado.id) {
+          return mudaDado;
+        }
+        return item;
+      })
+    );
+    setVisivel(false);
+  }
+
+  return (
+    <div>
+      <Table coluna={coluna} dataSource={menu} rowKey="id" />
+      <Modal
+        title="Editar Item"
+        visivel={visivel}
+        onOk={salvaTabela}
+        onCancel={() => setVisivel(false)}
+      >
+        <Input
+          value={mudaDado.name}
+          onChange={(e) =>
+            setMudaDado({ ...mudaDado, name: e.target.value })
+          }
+          placeholder="Nome"
+        />
+        <Input
+          value={mudaDado.description}
+          onChange={(e) =>
+            setMudaDado({ ...mudaDado, description: e.target.value })
+          }
+          placeholder="Descrição"
+        />
+        <Input
+          value={mudaDado.price}
+          onChange={(e) =>
+            setMudaDado({ ...mudaDado, price: e.target.value })
+          }
+          placeholder="Preço"
+        />
+      </Modal>
+    </div>
+  );
+}
+
+export default Restaurante;
+
+
+
+;
